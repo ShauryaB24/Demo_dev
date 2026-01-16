91e0ba15069298ddb8c971fb157f28e0e875a275
Shaurya <shauryab22@gmail.com>
2026-01-16 12:47:54 +0530

    DEMO-1: i have added a new file
    new file has been added for setup of github and commit message format so
    that there is always a consistency in the messages.I know this is
    frustrating but this will ensure that there is proper commits so that
    one can infere something valuabe form the message

---
+++ src/main/java/com/example/demoCommitMsg/DemoCommitMsgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoCommitMsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCommitMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCommitMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoCommitMsg/DemoCommitMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCommitMsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCommitMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
